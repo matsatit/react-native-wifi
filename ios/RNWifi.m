6df6003860bd8691caa2974b3cd3c508a41d2c16
flomair <f.mair@gmx.net>
2018-06-27 11:15:29 +0200

    Update RNWifi.m

--- ios/RNWifi.m
+++ ios/RNWifi.m
@@ -1,7 +1,12 @@
-#if TARGET_OS_IPHONE && !TARGET_OS_SIMULATOR
-
+#include "TargetConditionals.h"
+#if TARGET_IPHONE_SIMULATOR
 #import "RNWifi.h"
+@implementation WifiManager
+RCT_EXPORT_MODULE();
 
+@end
+#else
+#import "RNWifi.h"
 #import <SystemConfiguration/CaptiveNetwork.h>
 // If using official settings URL
 //#import <UIKit/UIKit.h>
@@ -15,11 +20,11 @@ RCT_EXPORT_MODULE();
 RCT_EXPORT_METHOD(connectToSSID:(NSString*)ssid
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
-    
+
     if (@available(iOS 11.0, *)) {
         NEHotspotConfiguration* configuration = [[NEHotspotConfiguration alloc] initWithSSID:ssid];
         configuration.joinOnce = true;
-        
+
         [[NEHotspotConfigurationManager sharedManager] applyConfiguration:configuration completionHandler:^(NSError * _Nullable error) {
             if (error != nil) {
                 reject(@"nehotspot_error", @"Error while configuring WiFi", error);
@@ -27,7 +32,7 @@ RCT_EXPORT_METHOD(connectToSSID:(NSString*)ssid
                 resolve(nil);
             }
         }];
-        
+
     } else {
         reject(@"ios_error", @"Not supported in iOS<11.0", nil);
     }
@@ -38,11 +43,11 @@ RCT_EXPORT_METHOD(connectToProtectedSSID:(NSString*)ssid
                   isWEP:(BOOL)isWEP
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
-    
+
     if (@available(iOS 11.0, *)) {
         NEHotspotConfiguration* configuration = [[NEHotspotConfiguration alloc] initWithSSID:ssid passphrase:passphrase isWEP:isWEP];
         configuration.joinOnce = true;
-        
+
         [[NEHotspotConfigurationManager sharedManager] applyConfiguration:configuration completionHandler:^(NSError * _Nullable error) {
             if (error != nil) {
                 reject(@"nehotspot_error", @"Error while configuring WiFi", error);
@@ -50,7 +55,7 @@ RCT_EXPORT_METHOD(connectToProtectedSSID:(NSString*)ssid
                 resolve(nil);
             }
         }];
-        
+
     } else {
         reject(@"ios_error", @"Not supported in iOS<11.0", nil);
     }
@@ -59,7 +64,7 @@ RCT_EXPORT_METHOD(connectToProtectedSSID:(NSString*)ssid
 RCT_EXPORT_METHOD(disconnectFromSSID:(NSString*)ssid
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
-    
+
     if (@available(iOS 11.0, *)) {
         [[NEHotspotConfigurationManager sharedManager] getConfiguredSSIDsWithCompletionHandler:^(NSArray<NSString *> *ssids) {
             if (ssids != nil && [ssids indexOfObject:ssid] != NSNotFound) {
@@ -70,15 +75,15 @@ RCT_EXPORT_METHOD(disconnectFromSSID:(NSString*)ssid
     } else {
         reject(@"ios_error", @"Not supported in iOS<11.0", nil);
     }
-    
+
 }
 
 RCT_REMAP_METHOD(getCurrentWifiSSID,
                  resolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject) {
-    
+
     NSString *kSSID = (NSString*) kCNNetworkInfoKeySSID;
-    
+
     NSArray *ifs = (__bridge_transfer id)CNCopySupportedInterfaces();
     for (NSString *ifnam in ifs) {
         NSDictionary *info = (__bridge_transfer id)CNCopyCurrentNetworkInfo((__bridge CFStringRef)ifnam);
@@ -87,7 +92,7 @@ RCT_REMAP_METHOD(getCurrentWifiSSID,
             return;
         }
     }
-    
+
     reject(@"cannot_detect_ssid", @"Cannot detect SSID", nil);
 }
 
@@ -98,11 +103,5 @@ RCT_REMAP_METHOD(getCurrentWifiSSID,
              };
 }
 @end
-#else
-#import "RNWifi.h"
-@implementation WifiManager
-RCT_EXPORT_MODULE();
 
-@end
 #endif
-
